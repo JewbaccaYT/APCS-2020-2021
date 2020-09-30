a58911e15bba0fca0a5d491a920e1f123ce3f766
JewbaccaYT <wookieofmagic@gmail.com>
2020-09-30 12:57:14 -0400

    Working on assignment

---
+++ EssayAssignment.py
@@ -0,0 +1,37 @@
+"String Variables Exercise"
+# ask a user to enter their first name and store it in a variable
+# ask a user to enter their last name and store it in a variable
+# print their full name
+# Make sure you have a space between first and last name
+# Make sure the first letter of first name and last name is uppercase
+# Make sure the rest of the name is lowercase
+
+"Numeric Variables Exercise"
+# Ask a user to enter a number
+# Ask a user to enter a second number
+# Calculate the total of the two numbers added, subtracted, multiplied and divided
+# Be sure to have a print statement for each operation
+
+"Combining Both to Create an Essay"
+# Give me an brief paragraph that will include details about you
+# Be sure to include variables for all colleges you are applying to
+# What your GPA is and a calculation that will demonstrate how to increase it
+# Finally, which activities you are involved in
+# The purpose of this will see how well you can generate a paragraph through print statements
+# string and numeric variables
+# Comments should include brief explanations of different sections of your code
+
+# Name Input and Print
+first_name = input("What is your first name?: ")
+last_name = input("What is your last name?: ")
+print(first_name.capitalize(), last_name.capitalize())
+
+# Number Input and Calculations
+first_num = float(input("Enter a number?: "))
+second_num = float(input("Enter another number?: "))
+print(first_num + second_num)
+print(first_num - second_num)
+print(first_num * second_num)
+print(first_num / second_num)
+
+# Essay
